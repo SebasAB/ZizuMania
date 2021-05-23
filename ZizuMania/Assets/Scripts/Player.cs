ec6b709dc97b5a03f187df044bc6f735d1068ba5
Sebastián Aliaga <sebasaliaga2515@gmail.com>
2021-05-23 09:27:07 -0500

    Basic movement added
    Basic horizontal movement added with the help of Unity Standard Assets

---
+++ ZizuMania/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f; 
+
+    Rigidbody2D myRigidBody;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run(); 
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+}
