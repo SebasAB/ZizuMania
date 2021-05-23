493d29330e64c307182ee46145688cb3c24b4cf3
Sebastián Aliaga <sebasaliaga2515@gmail.com>
2021-05-23 15:56:13 -0500

    Added cinemachine
    Added the cinemachine assets to work with the camera movement following the player

--- ZizuMania/Assets/Scripts/Player.cs
+++ ZizuMania/Assets/Scripts/Player.cs
@@ -7,7 +7,8 @@ public class Player : MonoBehaviour
 {
     // configs
     [SerializeField] float runSpeed = 5f;
-    [SerializeField] float jumpSpeed = 5f; 
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f; 
 
     // states 
     bool isAlive = true; 
@@ -15,18 +16,23 @@ public class Player : MonoBehaviour
 
     // cache components references
     Rigidbody2D myRigidBody;
-    Animator myAnimator; 
+    Animator myAnimator;
+    Collider2D myCollider2D;
+    float gravityScaleAtStart; 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
-        myAnimator = GetComponent<Animator>(); 
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale; 
     }
 
     void Update()
     {
         Run();
         Jump(); 
-        FlipSprite(); 
+        FlipSprite();
+        ClimbLadder(); 
     }
 
     private void Run()
@@ -39,14 +45,37 @@ public class Player : MonoBehaviour
         myAnimator.SetBool("Run", playerHasHorizontalSpeed);  
     }
 
+    private void ClimbLadder()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climb"))) 
+        {
+            myAnimator.SetBool("Climb", false);
+            myRigidBody.gravityScale = gravityScaleAtStart; 
+            return; 
+        }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+        myRigidBody.gravityScale = 0; 
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climb", playerHasVerticalSpeed); 
+    }
+
     private void Jump()
     {
+        // check if the player is touching the ground so it doesn't double jump
+
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; } 
+
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
             myRigidBody.velocity += jumpVelocityToAdd; 
         }
     }
+
 
     private void FlipSprite()
     {
